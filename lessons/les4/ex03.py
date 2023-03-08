d5fcbec0d4cfcf77ed0f8ee843db88b6b3f629d1
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2023-03-08 21:49:16 +0300

    lesson 4 added

---
+++ lessons/les4/ex03.py
@@ -0,0 +1,13 @@
+def calk2(a, b):
+    return a * b
+
+
+def math(op, x, y):
+    print(op(x, y))
+
+
+# calk1 = lambda a, b: a + b
+
+
+math(lambda a, b: a + b, 5, 10)
+math(calk2, 5, 10)
---
+++ lessons/les4/ex04.py
@@ -0,0 +1,32 @@
+# В списке хранятся числа.
+# Нужно выбрать только четные числа 
+# и составить список пар (число, квадрат числа)
+# Пример: 1 2 3 5 8 15 23 38
+# Получить: [(2,4), (8, 64), (38, 1444)]
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = list()
+
+# for i in data:
+#     if not i % 2:
+#         res.append((i, i ** 2))
+
+# print(res)
+
+# def select(f, col):
+#     return [f(x) for x in col]     # map
+
+# def where(f, col):
+#     return [x for x in col if f(x)]   # filter
+
+data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = select(int, data)
+res = map(int, data)
+# print(res)
+# res = where(lambda x: x % 2 == 0, res)
+res = filter(lambda x: x % 2 == 0, res)
+# print(res)
+
+# res = list(select(lambda x: (x, x ** 2), res))
+res = list(map(lambda x: (x, x ** 2), res))
+print(res)
